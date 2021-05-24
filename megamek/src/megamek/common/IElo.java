51308d2cc6bcdef3410f352d4cd62a5c7ca7b9d6
LorHil <73492424+LorHil@users.noreply.github.com>
2021-05-24 13:12:20 +0200

    Creation of IElo.java
    Interface IElo created - extends Serializable

---
+++ megamek/src/megamek/common/IElo.java
@@ -0,0 +1,13 @@
+package megamek.common;
+
+import megamek.server.Server;
+import java.io.Serializable;
+
+/**
+ * Date: 5/24/21
+ * Time: 10:05 AM
+ */
+
+public interface IElo extends Serializable {
+    
+}
